45f729bcfecdc15bedfe51f38f3ac79d0f842a8d
Zach Saunders <zachary.saunders@gmail.com>
2013-04-01 13:17:41 -0400

    Doc fix

--- Mustache/Mustache.php
+++ Mustache/Mustache.php
@@ -5,16 +5,15 @@ class Mustache {
     public static function Comb() {
         Laravel\Event::listen(View::engine, function($view)
         {
-            // The Blade view engine should only handle the rendering of views which
-            // end with the Blade extension. If the given view does not, we will
-            // return false so the View can be rendered as normal.
+            // Only Fire for .mustache files
             if ( ! str_contains($view->path, '.mustache'))
             {
                 return;
             }
 
+            // JSON_PRETTY_PRINT only avail in php 5.4+
             if ( ! defined('JSON_PRETTY_PRINT') )
-            define('JSON_PRETTY_PRINT', FALSE);
+                define('JSON_PRETTY_PRINT', FALSE);
 
             Laravel\Event::fire("laravel.composing: {$view->view}", array($view));
             Laravel\Log::info('<strong>Source Data (JSON Encoded) Passed to Template:</strong> ' . "<br><pre>" . json_encode($view->data, JSON_PRETTY_PRINT) . '</pre>');
@@ -22,8 +21,6 @@ class Mustache {
             ob_start();
 
             try {
-
-                // $m = new Engine(array('cache' => 'storage/cache', 'template_class_prefix' => 'Mustache_Template_'));
                 $m = new Engine(array(
                     'template_class_prefix' => 'Mustache_Template_',
                     'partials_loader' => new Loader\FilesystemLoader(path('app').'/views'),
